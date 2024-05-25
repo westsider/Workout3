e80ade089d6efc3187c7d22caceaa46b0c7efbcb
westsider <whansen1@mac.com>
2024-05-25 14:23:48 -0500

    Initial Commit

---
+++ Workout3/Workout3App.swift
@@ -0,0 +1,17 @@
+//
+//  Workout3App.swift
+//  Workout3
+//
+//  Created by Warren Hansen on 5/25/24.
+//
+
+import SwiftUI
+
+@main
+struct Workout3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
