3ed4c922de5e96e372aa48b821b465179d1c283f
westsider <whansen1@mac.com>
2024-05-26 11:45:49 -0500

    toggle set completed

--- Workout3/ExerciseGroup.swift
+++ Workout3/ExerciseGroup.swift
@@ -8,7 +8,8 @@
 //  X make a list of Bench, curls, fly's, lig lift,
 //  X items incluse num reps, weight and completed
 //  X ui has each item and num reps completed
-//  toggle set completed
+//  X toggle set completed
+
 //  when all completed return to main and show last workout
 //  list all workouts in main
 import SwiftUI
@@ -22,26 +23,28 @@ final class Exercise {
     var numSets: Int
     var weight: Int
     var completed: Bool
+    var date: Date
     
-    init(group: String, name: String, numReps: Int, numSets: Int, weight: Int, completed: Bool) {
+    init(group: String, name: String, numReps: Int, numSets: Int, weight: Int, completed: Bool, date: Date) {
         self.group = group
         self.name = name
         self.numReps = numReps
         self.numSets = numSets
         self.weight = weight
         self.completed = completed
+        self.date = date
     }
 }
 
 class DataLoader {
     func firstRun() -> [Exercise] {
-        let exercise1 = Exercise(group: "Group A", name: "Bench", numReps: 8, numSets: 4, weight: 135, completed: false)
+        let exercise1 = Exercise(group: "Group A", name: "Bench", numReps: 8, numSets: 4, weight: 135, completed: false, date: Date())
         
-        let exercise2 = Exercise(group: "Group A", name: "Curls", numReps: 8, numSets: 3, weight: 50, completed: false)
+        let exercise2 = Exercise(group: "Group A", name: "Curls", numReps: 8, numSets: 3, weight: 50, completed: false, date: Date())
         
-        let exercise3 = Exercise(group: "Group A", name: "Fly's", numReps: 8, numSets: 4, weight: 40, completed: false)
+        let exercise3 = Exercise(group: "Group A", name: "Fly's", numReps: 8, numSets: 4, weight: 40, completed: false, date: Date())
         
-        let exercise4 = Exercise(group: "Group A", name: "Leg Lifts", numReps: 8, numSets: 3, weight: 0, completed: false)
+        let exercise4 = Exercise(group: "Group A", name: "Leg Lifts", numReps: 8, numSets: 3, weight: 0, completed: false, date: Date())
         var exercises: [Exercise] = []
         exercises.append(exercise1)
         exercises.append(exercise2)
@@ -53,6 +56,7 @@ class DataLoader {
 
 struct ExerciseReps: View {
     var exercise: Exercise
+    @State var isOn: [Bool] = [false, false, false, false]
     var body: some View {
         VStack {
             ForEach(1...exercise.numSets, id: \.self) { num in
@@ -63,12 +67,14 @@ struct ExerciseReps: View {
                     Spacer()
                     Text("\(exercise.numReps) Reps")
                     Spacer()
-                    Image(systemName: "star")
+                    Toggle("", isOn: $isOn[num - 1])
+                                .toggleStyle(CheckToggleStyle())
                 }
             }
         }
     }
 }
+
 struct ExerciseGroup: View {
     
     @Environment(\.modelContext) private var modelContext
@@ -77,14 +83,13 @@ struct ExerciseGroup: View {
     
     var body: some View {
         
-        VStack {
+        VStack(alignment: .leading) {
             Text(exercise.first?.group ?? "no Group").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
             List(exercise)  { this in
                 Section(header: Text(this.name)) {
                     ExerciseReps(exercise: this)
                     
                 }.headerProminence(.increased)
-                
             }
         }.onAppear() {
             loadExercises()
@@ -114,4 +119,22 @@ struct ExerciseGroup: View {
     }
     return ExerciseGroup()
         .modelContainer(container)
+}
+
+struct CheckToggleStyle: ToggleStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        Button {
+            configuration.isOn.toggle()
+        } label: {
+            Label {
+                configuration.label
+            } icon: {
+                Image(systemName: configuration.isOn ? "star.circle.fill" : "star.circle")
+                    .foregroundStyle(configuration.isOn ? Color.accentColor : .primary)
+                    .accessibility(label: Text(configuration.isOn ? "Checked" : "Unchecked"))
+                    .imageScale(.large)
+            }
+        }
+        .buttonStyle(.plain)
+    }
 }
